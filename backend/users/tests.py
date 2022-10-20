2c0f6f0c22cddc6ec05eed32694177f1546b34fa
Alex Yu <alexy23@g.ucla.edu>
2022-10-20 12:37:24 -0700

    Add unit tests for profile

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+
+from profiles.models import Profile
 
 # Create your tests here.
+
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username='joebruin', email='jbruin@ucla.edu',
+                            first_name='Joe', last_name='Bruin')
+
+    def test_profile_dne(self):
+        """Profile should not exist yet for a new user and should raise a DoesNotExist exception"""
+        user = User.objects.get(username='joebruin')
+        self.assertRaises(Profile.DoesNotExist, Profile.objects.get, user=user)
+
+    def test_profile_creation(self):
+        """Profile gets created and is returned on GET request"""
+        client = APIClient()
+        user = User.objects.get(username='joebruin')
+        client.force_authenticate(user=user)
+
+        response = client.get(reverse('api:v1:users:profile'))
+        self.assertEqual(response.json(), {
+            'name': 'Joe',
+            'email': 'jbruin@ucla.edu',
+            'bio': ''
+        })
