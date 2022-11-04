dec5e5b6d20856aa92be8853d82c9643d9476398
Alex Yu <alexy23@g.ucla.edu>
2022-11-03 23:14:22 -0700

    Autogenerate API docs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
     'channels',
     'corsheaders',
+    'drf_yasg',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_jwt',
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,7 +16,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TeleTale API",
+        default_version='v1',
+        description="API scheme for the TeleTale backend.",
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(('api.urls', 'api'))),
+    path('docs/', schema_view.with_ui('redoc',
+                                      cache_timeout=0), name='schema-redoc'),
 ]
