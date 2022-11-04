958248c51d8e165ba8a7bfc43a0104390c7358a0
ColeTrammer <cole.trammer@gmail.com>
2022-11-03 18:35:29 -0700

    Add tests for the /users/profile/:id route

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -26,7 +26,7 @@ class ProfileTestCase(TestCase):
         """Profile data should be returned from a GET request to the /api/v1/users/profile endpoint"""
         client = APIClient()
         client.force_authenticate(user=self.user)
-        
+
         response = client.get(reverse('api:v1:users:profile'))
         self.assertEqual(response.json(), {
             'name': 'Joe',
@@ -45,4 +45,37 @@ class ProfileTestCase(TestCase):
     def test_profile_deleted_on_user_delete(self):
         """Profile should be deleted when its user is deleted"""
         self.user.delete()
-        self.assertRaises(Profile.DoesNotExist, Profile.objects.get, user=self.user)
+        self.assertRaises(Profile.DoesNotExist,
+                          Profile.objects.get, user=self.user)
+
+
+class QueryProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username='joebruin',
+            email='jbruin@ucla.edu',
+            first_name='Joe',
+            last_name='Bruin')
+        Profile.objects.create(
+            user=self.user,
+            display_name=self.user.first_name,
+            bio='Hello, there!',
+            profile_pic='')
+
+    def test_get_valid_profile(self):
+        """Profile data should be returned from a GET request to the /api/v1/users/profile/:id endpoint"""
+        client = APIClient()
+
+        response = client.get(f'/api/v1/users/profile/{self.user.id}')
+        self.assertEqual(response.json(), {
+            'name': 'Joe',
+            'bio': 'Hello, there!',
+            'profilePicture': ''
+        })
+
+    def test_get_invalid_profile(self):
+        """A 404 status code should be returned from /api/vi/users/profile/:id with a not found id"""
+        client = APIClient()
+
+        response = client.get('/api/v1/users/404')
+        self.assertEqual(response.status_code, 404)
