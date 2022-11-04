b4411ff6a4eaa32e9f40459d4a4ee5d5357c9472
ColeTrammer <cole.trammer@gmail.com>
2022-11-03 18:21:42 -0700

    Add backend /users/profile/:id route

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .apis  import UserProfileApi, UserInitApi
+from .apis import UserProfileApi, UserQueryProfileApi, UserInitApi
 
 
 urlpatterns = [
     path('profile/', UserProfileApi.as_view(), name='profile'),
+    path('profile/<int:id>', UserQueryProfileApi.as_view(), name='query_profile'),
     path('init/', UserInitApi.as_view(), name='init'),
 ]
